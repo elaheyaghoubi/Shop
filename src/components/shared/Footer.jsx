8320d283fe93344b30e7f6e67795edb1db8893f4
Elahe Yaghoubi <105369861+elaheyaghoubi@users.noreply.github.com>
2025-04-24 22:25:02 +0330

    footer added

---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <div className={"flex items-center justify-center font-medium text-[hsl(251,85%,36%)] bg-[rgba(131,190,226,1)] p-[1em] m-[1em] rounded-lg"}>
+                Created By: Stargirl
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -7,13 +7,25 @@ function Header() {
         <div className={"m-[1em] p-[1em] rounded-lg bg-[hsl(251,85%,36%)]"}>
             <ul className={"flex flex-row justify-between items-center w-[9%] flex-wrap"}>
                 <li>
-                    <div>Home</div>
+                    <div className={"text-white font-medium hover:cursor-pointer"}>Home</div>
                 </li>
                 <li>
-                    <FaShoppingCart />
+                    <div className={"relative p-[10px]"}>
+                        <FaShoppingCart
+                            size={22}
+                            className={"text-white hover:cursor-pointer "}
+                        />
+                        <div
+                        className={"absolute text-white hover:cursor-pointer bg-[#900707] text-xs text-center rounded-full top-0 right-0 p-[2px]"}>
+                            0
+                        </div>
+                    </div>
                 </li>
                 <li>
-                    <TbFolderHeart />
+                    <TbFolderHeart
+                        size={22}
+                        className={"text-white hover:cursor-pointer"}
+                    />
                 </li>
             </ul>
         </div>
