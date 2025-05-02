5b5335c6c17c85c2aa0467dc4fc8e7c08ca60bfa
Elahe Yaghoubi <105369861+elaheyaghoubi@users.noreply.github.com>
2025-05-02 19:27:11 +0330

    favorites page added

--- src/components/FavoritesPage/FavoritesPage.jsx
+++ src/components/FavoritesPage/FavoritesPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import FavoritesCard from "./FavoritesCard.jsx";
 import { removeFromFavorite } from "../../features/favoriteSlice.js";
-
+import {Link} from "react-router-dom";
 function FavoritesPage() {
     const favorites = useSelector((state) => state.favorites.favorites) || [];
 
@@ -20,7 +20,12 @@ function FavoritesPage() {
                     ))}
                 </div>
             ) : (
-                <div>No favorites found.</div>
+                <div className={"flex justify-center gap-2"}>
+                    <div className={"font-medium text-red-700"}>Your Favorite List Is Empty. </div>
+                    <Link to={"/"}>
+                        <div className={"text-amber-300 underline font-medium"}>Go To Shop</div>
+                    </Link>
+                </div>
             )}
         </div>
     );
